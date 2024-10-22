50b83668f411fee868e7328682855d45614b03ef
nolan.nieminen <nolan.nieminen@CZC222BX4L>
2024-10-22 08:56:25 +0300

    Add project files.

---
+++ Harjoitus 1/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace Harjoitus_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.VaihdaBT = new System.Windows.Forms.Button();
+            this.OtsikkoLB = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // VaihdaBT
+            // 
+            this.VaihdaBT.Location = new System.Drawing.Point(369, 149);
+            this.VaihdaBT.Name = "VaihdaBT";
+            this.VaihdaBT.Size = new System.Drawing.Size(75, 23);
+            this.VaihdaBT.TabIndex = 0;
+            this.VaihdaBT.Text = "button1";
+            this.VaihdaBT.UseVisualStyleBackColor = true;
+            this.VaihdaBT.Click += new System.EventHandler(this.VaihdaBT_Click);
+            // 
+            // OtsikkoLB
+            // 
+            this.OtsikkoLB.AutoSize = true;
+            this.OtsikkoLB.Location = new System.Drawing.Point(156, 153);
+            this.OtsikkoLB.Name = "OtsikkoLB";
+            this.OtsikkoLB.Size = new System.Drawing.Size(38, 15);
+            this.OtsikkoLB.TabIndex = 1;
+            this.OtsikkoLB.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.OtsikkoLB);
+            this.Controls.Add(this.VaihdaBT);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button VaihdaBT;
+        private Label OtsikkoLB;
+    }
+}
